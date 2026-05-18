8969c49e821511857186251ba1f6fda64685e66c
Võ Nhật Long <vonhatlongplm@gmail.com>
2026-05-18 10:25:25 +0700

    Add build configuration to vite.config.ts
    Added build configuration to specify output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,19 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    build: {
+      outDir: 'dist',
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
